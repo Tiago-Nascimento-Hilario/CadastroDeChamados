c905961733f94023a7b370eee5fd73e14152b025
Tiago-Nascimento-Hilario <tnhhilario@gmail.com>
2025-09-26 12:59:26 -0300

    Primeiro commit do projeto CadastroDeChamados

---
+++ src/main/java/com/dev/CadastroDeChamados/CadastroDeChamadosApplication.java
@@ -0,0 +1,13 @@
+package com.dev.CadastroDeChamados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeChamadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeChamadosApplication.class, args);
+	}
+
+}
